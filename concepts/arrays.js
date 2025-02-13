44c48ec8d0be470997d1f1fb23a77c8d5e942ca7
Ifeoluwaexam2025 <iogungbe24@students.oamk.fi>
2025-02-13 21:59:47 +0200

    added code examples

---
+++ concepts/arrays.js
@@ -0,0 +1,17 @@
+// To create arrays in js(javascript) we use the square bracket []
+let myFirstArray=[
+    'banana',
+    'chicken',
+    'apple','duck'
+]
+// Use console.log() to print to console
+console.log(myFirstArray)
+// To print different elements in the array use [0],[1],[2] with [0] being the first element.
+console.log(myFirstArray[0])
+console.log(myFirstArray[1])
+console.log(myFirstArray[2])
+console.log(myFirstArray[3])
+// use nameofarray.push() to add new elements to the array.
+myFirstArray.push('animal')
+console.log(myFirstArray)
+
---
+++ concepts/variable.js
@@ -0,0 +1,7 @@
+// To create variables in javascript we use the keywords var,let, or const
+var newWord= 'Chicken'
+var number = 5;
+const number2= 3 ;
+const newWord2='Turkey'
+let newWord3='Duck'
+let number3=4;
